33bb2103f2de453991771cef045bf60838948c15
Eamonn Maguire <eamonnmag@gmail.com>
2011-03-10 18:39:53 +0000

    Upgrade parent to bioinvindex 1.3.1

--- import_layer/run_tests.sh
+++ import_layer/run_tests.sh
@@ -1,5 +1,3 @@
-mvn2 clean test -DargLine="-Xms256m -Xmx1024m -XX:PermSize=64m -XX:MaxPermSize=512m" \
+mvn clean test -DargLine="-Xms256m -Xmx1024m -XX:PermSize=64m -XX:MaxPermSize=512m" \
     -Dsurefire.useFile=true \
-    -Ph2,test 
-#-Dtest=ISAConfigurationBatchTest
-mvn -Ph2,test surefire-report:report-only
+    -Ph2,test
