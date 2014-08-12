4e89f776ebf4f53a0da9bafc8f5efc823480469d
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2014-08-12 14:35:00 +0100

    Downloading latest configurations in package script

--- val_conv_manager_gui/package.sh
+++ val_conv_manager_gui/package.sh
@@ -5,8 +5,60 @@
 
 #MVNOPTS="--offline"
 
-pwd=$(dirname $0)
-cd $pwd
+function pause(){
+   read -p "$*"
+}
+
+PACKAGE_TYPE=$1
+echo "ISAvalidator-ISAconverter-BIImanager packaging for type " $PACKAGE_TYPE
+
+if [ "$PACKAGE_TYPE" = "mixs"  ]
+then
+    CONFIGURATION=isaconfig-mixs-v4.zip
+else
+    CONFIGURATION=isaconfig-default_v2014-01-16.zip
+fi
+
+echo "Configuration file: " $CONFIGURATION
+
+#pause "check configuratio filename and press enter"
+
+if hash curl 2>/dev/null; then
+   echo "curl is installed, will download configurations next"
+else
+   echo "curl is not installed, install it and then run package.sh again"
+   exit 1
+fi
+
+curl -L -O http://bitbucket.org/eamonnmag/isatools-downloads/downloads/"$CONFIGURATION"
+
+mv $CONFIGURATION ../config
+
+WD=$(pwd)
+
+pwd
+cd ../config
+unzip $CONFIGURATION
+
+filename="${CONFIGURATION%.*}"
+
+echo $filename
+
+#pause "Check filename"
+
+mv $filename default-config
+
+#pause "Renamed file"
+
+rm -f $CONFIGURATION
+pwd
+cd $WD
+echo "Changing back to target..."
+pwd
+
+
+#pwd=$(dirname $0)
+#cd $pwd
 
 cd ../import_layer
 mvn $MVNOPTS -Ptools -Dmaven.test.skip=true clean install
